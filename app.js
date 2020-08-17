59d8fe23a57c9c59c9a19f48841bfff728785eb3
sexring <69797994+sexring@users.noreply.github.com>
2020-08-17 17:32:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyCJc1odH1xAOYK1eK-XISWfuHgyaFSDy4w",
+  authDomain: "face-9ccbc.firebaseapp.com",
+  databaseURL: "https://face-9ccbc.firebaseio.com",
+  projectId: "face-9ccbc",
+  storageBucket: "face-9ccbc.appspot.com",
+  messagingSenderId: "286412588827",
+  appId: "1:286412588827:web:dffaa055969bcaca4875be",
+  measurementId: "G-K02067TM4G"
 };
 
      
